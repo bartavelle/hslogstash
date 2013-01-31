81b8bab2b1befdb447a21e77713368805e825dd6
bartavelle <bartavelle@gmail.com>
2013-01-31 18:49:19 +0100

    Modified the logstash source API

--- Data/Conduit/Logstash.hs
+++ Data/Conduit/Logstash.hs
@@ -11,13 +11,16 @@ import Codec.Text.IConv
 import Data.Text.Encoding
 import Logstash.Message
 
-tryDecode :: (FromJSON a) => BS.ByteString -> Maybe a
+tryDecode :: (FromJSON a) => BS.ByteString -> Either BS.ByteString a
 tryDecode i =
     let latin1 = convert "LATIN1" "UTF-8" li
         li = BSL.fromStrict i
-    in  case decodeUtf8' i of
-            Left _  -> decode latin1
-            Right _ -> decode li
+        o = case decodeUtf8' i of
+                Left _  -> decode latin1
+                Right _ -> decode li
+    in case o of
+           Just x  -> Right x
+           Nothing -> Left i
 
-logstashListener :: Int -> Sink LogstashMessage (ResourceT IO) () -> IO ()
-logstashListener port sink = runResourceT $ runTCPServer (serverSettings port HostAny) (\app -> appSource app $= CB.lines $= CL.mapMaybe tryDecode $$ sink)
+logstashListener :: Int -> Sink (Either BS.ByteString LogstashMessage) (ResourceT IO) () -> IO ()
+logstashListener port sink = runResourceT $ runTCPServer (serverSettings port HostAny) (\app -> appSource app $= CB.lines $= CL.map tryDecode $$ sink)
