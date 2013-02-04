663bf8f5e6058b476c9ed9b5c9cf087221b79b36
bartavelle <bartavelle@gmail.com>
2013-02-04 14:37:30 +0100

    Make the Redis source timeout tunable.

--- Data/Conduit/Redis.hs
+++ Data/Conduit/Redis.hs
@@ -15,9 +15,12 @@ import Data.Maybe (catMaybes)
 import Control.Exception
 import Control.Concurrent hiding (yield)
 
+import Debug.Trace
+
 mp :: Either Reply (Maybe (BS.ByteString, BS.ByteString)) -> Maybe BS.ByteString
 mp (Right (Just (_, x))) = Just x
-mp _ = Nothing
+mp (Right Nothing) = Nothing
+mp x = trace (show x) Nothing
 
 safePush :: BS.ByteString -> MVar Connection -> ConnectInfo -> BS.ByteString -> IO ()
 safePush list mconn cinfo input = catch mypush (\SomeException{} -> resetMVar)
@@ -36,22 +39,26 @@ safePush list mconn cinfo input = catch mypush (\SomeException{} -> resetMVar)
                 Right _ -> return ()
                 err -> BS.putStrLn ("retrying ... " `BS.append` BS.pack (show err)) >> threadDelay 500000 >> safePush list mconn cinfo input
 
-popN :: BS.ByteString -> Int -> Redis [BS.ByteString]
-popN l n = do
-    f <- fmap mp (brpop [l] 0)
-    nx <- fmap rights $ replicateM (n-1) (rpop l)
-    return $ catMaybes (f:nx)
+popN :: BS.ByteString -> Int -> Integer -> Redis [BS.ByteString]
+popN l n to = do
+    f <- fmap mp (brpop [l] to)
+    if f == Nothing
+        then return [] -- short circuiting
+        else do
+            nx <- fmap rights $ replicateM (n-1) (rpop l)
+            return $ catMaybes (f:nx)
 
 redisSource :: (MonadResource m)
             => HostName         -- ^ Hostname of the Redis server
             -> Int              -- ^ Port of the Redis server (usually 6379)
             -> BS.ByteString    -- ^ Name of the list
             -> Int              -- ^ Number of elements to pop at once
+            -> Integer          -- ^ Timeout of the brpop function in seconds, useful for getting Flush events into your conduit. Set to 0 for no timeout.
             -> Source m [BS.ByteString]
-redisSource h p list nb =
+redisSource h p list nb to =
     let cinfo = defaultConnectInfo { connectHost = h, connectPort = PortNumber $ fromIntegral p }
         myPipe :: (MonadResource m) => Connection -> Source m [BS.ByteString]
-        myPipe conn = forever (liftIO (runRedis conn (popN list nb)) >>= yield)
+        myPipe conn = forever (liftIO (runRedis conn (popN list nb to)) >>= yield)
     in  bracketP (connect cinfo) (\conn -> runRedis conn (void quit)) myPipe
 
 -- | Warning, this outputs strings when things go wrong!
