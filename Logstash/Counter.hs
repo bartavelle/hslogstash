bacc49a963818ecc15b43708d52df50f8bdcee2d
bartavelle <bartavelle@gmail.com>
2013-08-27 14:31:06 +0200

    Fix collectd communication

--- Logstash/Counter.hs
+++ Logstash/Counter.hs
@@ -10,6 +10,9 @@ import Network.Socket
 import Control.Monad
 import Control.Exception
 import Data.Time.Clock.POSIX
+import qualified Data.Text as T
+import qualified Data.Text.IO as T
+import Data.Monoid
 
 newtype Counter = Counter (TVar Integer)
 
@@ -33,6 +36,7 @@ counter2collectd :: Counter  -- the counter to export
                  -> IO ()
 counter2collectd c sockpath nodename plugin vinstance = void $ forkIO work
     where
+        hdr = "PUTVAL " <> T.pack nodename <> "/" <> T.pack plugin <> "/derive-" <> T.pack vinstance <> " interval=10 "
         collectdConnect :: IO Handle
         collectdConnect = do
                 soc <- socket AF_UNIX Stream 0
@@ -41,8 +45,9 @@ counter2collectd c sockpath nodename plugin vinstance = void $ forkIO work
         work = do
             bracket collectdConnect hClose $ \h -> do
                 v  <- readCounter c
-                tt <- fmap (show . (truncate :: (RealFrac a) => a -> Integer)) getPOSIXTime
-                hPutStrLn h ("PUTVAL " ++ nodename ++ "/" ++ plugin ++ "/derive-" ++ vinstance ++ " interval=10 " ++ show tt ++ ":" ++ show v)
+                tt <- fmap (T.pack . show . (truncate :: (RealFrac a) => a -> Integer)) getPOSIXTime
+                T.hPutStrLn h (hdr <> tt <> ":" <> T.pack (show v))
+                T.putStrLn (hdr <> tt <> ":" <> T.pack (show v))
             threadDelay 10000000
             work
 
