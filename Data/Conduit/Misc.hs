8e495cdde878c5c49c736da45def301d824edd6f
bartavelle <bartavelle@gmail.com>
2013-10-14 15:14:40 +0200

    Generalized the flushing conduit API.

--- Data/Conduit/Misc.hs
+++ Data/Conduit/Misc.hs
@@ -10,18 +10,43 @@ concat = awaitForever (mapM_ yield)
 -- | Converts a stream of [a] into a stream of (Flush a). This is done by
 -- sending a Flush when the input is the empty list, or that we reached
 -- a certain threshold
-concatFlush :: (Monad m) => Integer -> Conduit [a] m (Flush a)
-concatFlush mx = concatFlush' 0
+simpleConcatFlush :: (Monad m) => Integer -> Conduit [a] m (Flush a)
+simpleConcatFlush mx = concatFlush 0 sendf
     where
-        concatFlush' x = await >>= maybe (return ()) (\input -> if null input
-                                                                    then yield Flush >> concatFlush' 0
-                                                                    else foldM sendf x input >>= concatFlush'
-                                                  )
         sendf curx ev = do
             yield (Chunk ev)
             if curx >= mx
                 then yield Flush >> return 0
                 else return (curx+1)
+
+-- | This is a more general version of 'simpleConcatFlush', where you
+-- provide your own fold.
+concatFlush :: (Monad m) => b -> (b -> a -> ConduitM [a] (Flush a) m b) -> Conduit [a] m (Flush a)
+concatFlush initial foldfunc = concatFlush' initial
+    where
+        concatFlush' x = await >>= maybe (return ()) (\input -> if null input
+                                                                    then yield Flush >> concatFlush' initial
+                                                                    else foldM foldfunc x input >>= concatFlush')
+
+-- | A generalized version of 'simpleConcatFlush' where some value is
+-- summed and the 'Flush' is sent when it reaches a threshold.
+concatFlushSum :: Monad m
+               => (a -> Integer) -- ^ Convert your input value into an Integer, usually a size
+               -> Integer -- ^ The threshold value
+               -> Conduit [a] m (Flush a)
+concatFlushSum tolength maxlength = concatFlush 0 foldfunc
+    where
+        foldfunc curlength element = do
+            let nextlength = curlength + elementlength
+                elementlength = tolength element
+            if nextlength > maxlength
+                then do
+                    yield Flush
+                    yield (Chunk element)
+                    return elementlength
+                else do
+                    yield (Chunk element)
+                    return nextlength
 
 -- | Regroup a stream of (Flush a) into a stream of lists, using "Flush" as
 -- the separator
