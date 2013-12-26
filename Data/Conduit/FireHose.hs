af128d015da733004c4e92f915c16300f14c7ac2
bartavelle <bartavelle@gmail.com>
2013-12-26 12:55:25 +0100

    added firehose

--- Data/Conduit/FireHose.hs
+++ Data/Conduit/FireHose.hs
@@ -2,21 +2,30 @@
 -- output.
 module Data.Conduit.FireHose (fireHose) where
 
-import Control.Concurrent
-import Control.Concurrent.STM
-import Data.Conduit
-import Data.Conduit.TMChan
-import Data.Conduit.Network
-import Control.Monad
 import Control.Monad.IO.Class
-import qualified Data.ByteString as BS
+
+import Logstash.Message
+
+import Data.Conduit
+import Data.Conduit.Network.Firehose
+import Network.Wai (Request,pathInfo)
+
+import qualified Data.HashSet as HS
+import qualified Data.Text as T
+
+import Data.Aeson
+import Blaze.ByteString.Builder.ByteString
 
 -- | All clients connecting to the supplied port will start getting the
 -- input of this conduit.
 fireHose :: MonadIO m => Int -- ^ Port
-                      -> IO (Sink BS.ByteString m ())
-fireHose port = do
-    chan <- atomically $ newTBMChan 16
-    void $ forkIO $ runTCPServer (serverSettings port HostAny) (\ad -> sourceTBMChan chan $$ appSink ad)
-    return (sinkTBMChan chan)
+                      -> Int -- ^Buffer size for the fire hose threads
+                      -> IO (Conduit LogstashMessage m LogstashMessage)
+fireHose port buffersize = firehoseConduit port buffersize getFilter serialize
+    where
+        serialize = fromLazyByteString . encode
+        getFilter r = case pathInfo r of
+                          [p] -> let set = HS.fromList $ T.splitOn "," p
+                                 in  flip HS.member set . logstashType
+                          _ -> error "invalid url"
 
